35dfb6d543718c918b2b5aadcbdc3837b2b0b98d
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2023-01-19 20:25:16 -0500

    Switch to 'Following' if user goes to tab other than home first'

---
+++ README.md
@@ -0,0 +1,3 @@
+# Load 'Following' tab by default on Twitter
+
+Is the 'For You' just not for you? Tired of clicking on the 'Following Tab' every time you load Twitter? Here's a simple extension that does it for you, everytime you load twitter
--- auto_following_tab.js
+++ auto_following_tab.js
@@ -1,10 +1,28 @@
-console.log("start");
 window.addEventListener("load", (event) => {
-    console.log("page loaded func start");
+
+    const followingAction = () => {
+        const spanFollowing = document.querySelector("a.r-6416eg[tabindex='-1']");
+        spanFollowing.click();
+    }
+
     setTimeout(() => {
-        const spanFollowing = document.querySelectorAll("a.r-6416eg[tabindex='-1']")[0];
-        spanFollowing.click();        
+        // perform switch when landing on home
+        if (window.location.href == "https://twitter.com/home") {
+            followingAction();
+        }
+        
+        // if on any tab other than home, grab the home tab element and create click event
+        else {
+            const homeTab = document.querySelector("a[data-testid='AppTabBar_Home_Link']");
+            homeTab.addEventListener("click", function handler() {
+                setTimeout(() => {
+                    followingAction();
+                    // remove listener to prevent switching between 'For You'/'Following' everytime
+                    // user goes to home tab
+                    homeTab.removeEventListener("click", handler);
+                }, 1500);  // wait 1.5 seconds for 'Following' tab to build
+            });
+        }
     }, 1000);
-    console.log("page loaded func done");
+
 });
-console.log("done");
